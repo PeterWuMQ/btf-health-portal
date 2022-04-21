57b80eff7c7d49c1b1337ae11030453c50225d91
PeterWuMQ <80020886+PeterWuMQ@users.noreply.github.com>
2022-04-21 15:24:30 +1000

    added backend express API skeleton

--- server/server.js
+++ server/server.js
@@ -1,3 +1,36 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const fs = require('fs')
+
+var corsOptions = {
+    origin: "*",
+}
+
+app.use(express.json())
+app.use(cors(corsOptions))
+
+let fileData = fs.readFileSync('server/questions.json')
+let questions = JSON.parse(fileData)
+
+fileData = fs.readFileSync('server/results.json')
+let results = JSON.parse(fileData)
+
+fileData = fs.readFileSync('server/recommendations.json')
+let recommendations = JSON.parse(fileData)
+
+
+app.get('/api/questions', (request, response) => {
+    response.send(questions)
+})
+
+app.get('/api/results', (request, response) => {
+    response.send(results)
+})
+
+app.get('/api/recommendations', (request, response) => {
+    response.send(recommendations)
+})
+
+const PORT = 3001
+app.listen(PORT, () => (console.log('Server running on 3001')))
