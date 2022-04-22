7efd3f5554d6e6fe7cd1c2cf4079789230d7b4ab
PeterWuMQ <80020886+PeterWuMQ@users.noreply.github.com>
2022-04-23 00:59:33 +1000

    Made dynamic Result Table code even more efficient

--- src/components/table/TableRow.js
+++ src/components/table/TableRow.js
@@ -8,13 +8,12 @@ function TableRow ({rowItems, result, index}) {
     return (
         <tr>
             {result ? rowItems.map((r, i) => {
-                if (i !== 0) {
-                    var temp = r.match(/[0-9]+/g)
-                    return <td bgcolor={(temp.length === 1 && result[0][i - 1] >= temp[0]) || (result[0][i - 1] >= temp[0] && result[0][i - 1] <= temp[1]) ? colour : null} key={i + ".1"}><NormalText key={i + ".2"}> {r} </NormalText></td>
-                } else {
-                    return <td key={i + ".1"}><NormalText key={i + ".2"}> {r} </NormalText></td>
-                }
-
+                var temp = r.match(/[0-9]+/g)
+                return i !== 0 
+                ? (temp.length === 1 && result[0][i - 1] >= temp[0]) || (result[0][i - 1] >= temp[0] && result[0][i - 1] <= temp[1])
+                ? <td bgcolor={colour} key={i + ".1"}><NormalText key={i + ".2"}> {r} </NormalText></td>
+                : <td key={i + ".1"}><NormalText key={i + ".2"}> {r} </NormalText></td>
+                : <td key={i + ".1"}><NormalText key={i + ".2"}> {r} </NormalText></td>
             })
             : rowItems.map((r, i) => <td key={i + ".1"}><NormalText key={i + ".2"}> {r} </NormalText></td>)}
         </tr>
