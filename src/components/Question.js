afa1514149c830bd78616ba57652fc960e67d667
PeterWuMQ <80020886+PeterWuMQ@users.noreply.github.com>
2022-04-24 17:34:18 +1000

    set placeholder structure for results and recommendations

--- src/components/Question.js
+++ src/components/Question.js
@@ -14,7 +14,7 @@ function Question ({qid, question, tempAnswers, answers, setAnswers, type}) {
             ? <Select name={option} id={qid} answers={answers} type={""} tempAnswers={tempAnswers} setAnswers={setAnswers}/>
             : answers.map((a, i) => {
                 const id = qid + "." + i
-                return<Answer key={i} id={id} name={option} tempAnswers={tempAnswers} setAnswers={setAnswers} qid={qid} type={type} value={a[0]} checked={i === 0 ? true : false}> {a[1]} </Answer>
+                return <Answer key={i} id={id} name={option} tempAnswers={tempAnswers} setAnswers={setAnswers} qid={qid} type={type} value={a[0]} checked={i === 0 ? true : false}> {a[1]} </Answer>
                 })
             }
         </div>
